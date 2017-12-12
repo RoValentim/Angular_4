22525cd83cbc0679f9b1b38e447abb889aa4bd07
Rodrigo Valentim <rodrigo.valentim@agilitynetworks.com.br>
2017-12-12 17:47:38 -0200

    Angular Router Basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,8 +12,13 @@ import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 import 'hammerjs';
 
@@ -23,7 +28,10 @@ import 'hammerjs';
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -31,7 +39,8 @@ import 'hammerjs';
     FlexLayoutModule,
     FormsModule,
     HttpModule,
-    MaterialModule
+    MaterialModule,
+    AppRoutingModule
   ],
   providers: [DishService],
   bootstrap: [AppComponent]
